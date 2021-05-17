c5ed3a21774f745bd58e158f7e56482ac9ef0f7e
Manuel <contact@sanramonmanuel.com>
2021-05-17 22:05:30 +0800

    COMBINED CSS

---
+++ _all-themes.js
@@ -0,0 +1,25 @@
+/* Trash Butler Branding */
+KnackInitAsync = function($, callback) {
+    LazyLoad.css(['https://dl.dropbox.com/s/neddkanwpftf13x/kn-TB-theme.min.css'], callback);
+};
+
+// Trash Butler Backup Service
+KnackInitAsync = function($, callback) {
+    LazyLoad.css(['https://dl.dropbox.com/s/udouvvkxmi450h9/Trash-Butler-theme.min.css'], callback);
+};
+
+// Onboarding App
+KnackInitAsync = function($, callback) {
+    LazyLoad.css(['https://dl.dropbox.com/s/s4kuytwvo7292kj/Onboarding-Theme.min.css'], callback);
+};
+
+
+/* Hunk Pro Branding */
+KnackInitAsync = function($, callback) {
+    LazyLoad.css(['https://dl.dropbox.com/s/27q4q37d35q8cwf/kn-CHHJ-theme.min.css'], callback);
+};
+
+// Claims App
+KnackInitAsync = function($, callback) {
+    LazyLoad.css(['https://dl.dropbox.com/s/jqqg6ymyadelbmp/Claims-App_Theme.min.css'], callback);
+};
